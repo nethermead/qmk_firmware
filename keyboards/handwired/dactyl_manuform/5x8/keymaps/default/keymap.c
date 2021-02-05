c86c17018d1a9516f55e6525c99c1e59c544977a
Andy Langton <nethermead@gmail.com>
2021-02-05 17:58:15 -0500

    workable

--- keyboards/handwired/dactyl_manuform/5x8/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x8/keymaps/default/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    _______, _______, KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,
    _______, _______, KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,
    _______, _______, OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-   _______, _______, OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
+   _______, _______, KC_LCTL,   KC_LALT,    KC_X,    KC_C,   KC_SPC,   KC_BSPC,
                                KC_END, KC_HOME,
                                KC_PSCR, TASK,
         // right hand
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,  _______, _______,
                      KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  _______, _______,
                      KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  OSM(MOD_RSFT),  KC_UP,  _______,
-                     _______,  _______, _______, _______,  _______,  KC_LEFT, KC_DOWN,  KC_RIGHT,
+                     KC_DEL,  KC_ENT, _______, _______,  _______,  KC_LEFT, KC_DOWN,  KC_RIGHT,
                     KC_BSPC, KC_ENT,
                     KC_LCTL, KC_LALT),
 
