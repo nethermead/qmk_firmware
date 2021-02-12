38102b19f49e6b8fe47f17ee32e7c0d646dec43b
Andrew Langton <nethermead@gmail.com>
2021-02-11 19:51:29 -0500

    tweak to keymap

--- keyboards/handwired/dactyl_manuform/5x8/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x8/keymaps/default/keymap.c
@@ -19,21 +19,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_5x8(
   // left hand
-   _______, _______, KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5,
-   _______, _______, KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,
-   _______, _______, KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,
-   _______, _______, OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-   _______, _______, KC_LCTL,   KC_LALT,    KC_X,    KC_C,   KC_SPC,   KC_BSPC,
-                               KC_END, KC_HOME,
-                               KC_PSCR, TASK,
+   KC_F1, KC_F2, KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5,
+   KC_F3, KC_F4, KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,
+   KC_F5, KC_F6, KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,
+   KC_F7, KC_F8, OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
+   KC_F9, KC_F10, KC_LCTL,   KC_LALT,    KC_LGUI,    KC_APP,
+                               KC_SPC,   KC_BSPC,
+                               KC_PGUP, KC_HOME,
+                               KC_PGDN, KC_END,
         // right hand
                      KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_GRV,
-                     KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,  _______, _______,
-                     KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  _______, _______,
-                     KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  OSM(MOD_RSFT),  KC_UP,  _______,
-                     KC_DEL,  KC_ENT, _______, _______,  _______,  KC_LEFT, KC_DOWN,  KC_RIGHT,
-                    KC_BSPC, KC_ENT,
-                    KC_LCTL, KC_LALT),
+                     KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
+                     KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_F11, KC_F12,
+                     KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  OSM(MOD_RSFT),  KC_UP,  KC_PSCR,
+                KC_BSPC,  KC_ENT,       KC_RGUI, KC_RALT,  KC_RCTL,  KC_LEFT, KC_DOWN,  KC_RIGHT,
+                KC_INS, KC__VOLUP,
+                KC_DEL, KC__VOLDOWN),
 
 /*[_FN] = LAYOUT_5x8(
   // left hand
